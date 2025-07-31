e6ac23e594d4c3c4b928d0527ef6c8a6b58edea7
Jentelprog <ilyeschanchah@gmail.com>
2025-07-31 12:06:08 +0100

    voices and text

--- app.py
+++ app.py
@@ -10,8 +10,9 @@ import pyautogui
 import json
 from duckduckgo_search import DDGS
 from google import genai
+import whisper
 
-#search function it open the browser and search for the content that you gave it in the input
+# search function it open the browser and search for the content that you gave it in the input
 def search(search):
     goToDesk()
     pyautogui.screenshot("screenshot.png")
@@ -43,18 +44,18 @@ def ducksearch(prompt, max_results=2):
     i = 1
     links = []
     for result in results:
-        # speak(f"the link number{i}")
-        # speak(f"the title is {result['title']}")
+        # googleSpeak(f"the link number{i}")
+        # googleSpeak(f"the title is {result['title']}")
         print(result["href"])  # Note: it's "href" not "link" in the current version
         links.append(result["href"])
-        # speak(result["body"])
+        # googleSpeak(result["body"])
         i += 1
-    # speak("wich one do you chose")
+    # googleSpeak("wich one do you chose")
     # n=recognizer()
-    # speak(n)
+    # googleSpeak(n)
     search(links[1])
 
-#recognizer is a function used to reconize the speach from the user and turn it to text
+# recognizer is a function used to reconize the speach from the user and turn it to text
 def recognizer():
     with sr.Microphone() as source:
         recognizer = sr.Recognizer()
@@ -74,22 +75,46 @@ def recognizer():
     return text
 
 # speak is used to simply tts
-def speak(text):
+def googleSpeak(text):
     myobj = gTTS(text=text, lang="en", slow=False)
     myobj.save("speach.mp3")
     sound = AudioSegment.from_mp3("speach.mp3")
     sound.export("speach.wav", format="wav")
     winsound.PlaySound("speach.wav", winsound.SND_FILENAME)
 
+from dotenv import load_dotenv
+from elevenlabs.client import ElevenLabs
+from elevenlabs import play
+import os
+
+def elevenSpeak(text):
+    load_dotenv()
+    elevenlabs = ElevenLabs(
+        api_key="sk_ee75c509c198d3cd439306748bebf1f4fa6ebee953800925",
+    )
+
+    audio = elevenlabs.text_to_speech.convert(
+        text=text,
+        voice_id="tnSpp4vdxKPjI9w0GnoV",
+        model_id="eleven_multilingual_v2",
+    )
+
+    play(audio)
+
+def wisper(audio_file):
+    model = whisper.load_model("turbo")
+    result = model.transcribe(audio_file)
+    return result["text"]
+
 # append_json is used to append the json file that is used as history
 def append_json(field, content):
-    with open("history.json", "r") as f:
+    with open("history.json", "r",encoding="UTF-8") as f:
         json_file = json.load(f)
     json_file[field].append(content)
-    with open("history.json", "w") as f:
-        json.dump(json_file, f)
+    with open("history.json", "w",encoding="UTF-8") as f:
+        json.dump(json_file, f, indent=4)
 
-#chat is used to comunicate with the server that is hosting the lm model in case you want to talk with the girlfriend
+# chat is used to comunicate with the server that is hosting the lm model in case you want to talk with the girlfriend
 def chat(text):
     client = OpenAI(base_url="http://127.0.0.1:1234/v1", api_key="lm-studio")
     models = [
@@ -110,9 +135,11 @@ def chat(text):
     )
     return response.choices[0].message.content
 
-#gemini is used to comunicate with gemini api as an alternative for the local server
+# gemini is used to comunicate with gemini api as an alternative for the local server
 def gemini(text):
-    with open(r"C:\Users\ilyes\OneDrive\Desktop\api.txt", "r") as f:
+    with open(
+        r"C:\Users\ilyes\OneDrive\Desktop\api.txt", "r"
+    ) as f:  # change this part so you have your api key here
         api_key = f.readline()
 
     client = genai.Client(api_key=api_key)
@@ -120,14 +147,14 @@ def gemini(text):
     response = client.models.generate_content(model="gemini-2.5-flash", contents=text)
     return response.text
 
-#resp is used to choose what the bot should do based on your input
+# resp is used to choose what the bot should do based on your input
 def resp(text):
     if text != "Sorry, I could not understand the audio.":
         if "my screen" in text:
             print("capture")
             pyautogui.screenshot("screenshot.png")
         elif "search" in text:
-            speak("what do you want me to search for")
+            googleSpeak("what do you want me to search for")
             prompt = recognizer()
             # prompt = "cats memes"
             ducksearch(prompt)
@@ -136,17 +163,20 @@ def resp(text):
                 f"do you want to talk with the chatbot(it's slow)\n you said:{text}"
             )
             if confirm == "OK":
-                response = gemini(text)
+                prompt=f"you will pretend that you are my girlfriend and assistance don't munchin it unless i ask you about it you love me to death. this is the prompt : {text}"
+                response = gemini(prompt)
                 h = {"user": text, "bot": response}
-                # append_json("chat", h)
-                print(str(response))
-                speak(str(response))
+                append_json("chat", h)
+                print(str(response).encode("utf-8"))
+                elevenSpeak(str(response))
 
 
 if __name__ == "__main__":
     text = ""
     while text != "close":
         text = recognizer()
+        if text=="close":
+            break
         if text != "Sorry, I could not understand the audio.":
             # text = input("user: ")
             resp(text)
--- test.py
+++ test.py
@@ -1,11 +1,57 @@
-from google import genai
-def gemini(text):
-    with open("api.txt","r") as f:
-        api_key=f.readline()
+# # # # from google import genai
+# # # # def gemini(text):
+# # # #     with open("api.txt","r") as f:
+# # # #         api_key=f.readline()
 
-    client = genai.Client(api_key=api_key)
+# # # #     client = genai.Client(api_key=api_key)
 
-    response = client.models.generate_content(
-        model="gemini-2.5-flash", contents="hi how are you "
-    )
-    return response.text
+# # # #     response = client.models.generate_content(
+# # # #         model="gemini-2.5-flash", contents="hi how are you "
+# # # #     )
+# # # #     return response.text
+# # # import whisper
+# # # def wisper(audio_file):
+# # #     model = whisper.load_model("turbo")
+# # #     result = model.transcribe(audio_file)
+# # #     return result["text"]
+
+# # # text=wisper("Injuries & Being Sick.mp3")
+# # # with open("jaden_video.txt",'w') as f:
+# # #     f.write(text)
+# # from RealtimeSTT import AudioToTextRecorder
+# # import pyautogui
+
+# # def process_text(text):
+# #     pyautogui.typewrite(text + " ")
+
+# # if __name__ == '__main__':
+# #     print("Wait until it says 'speak now'")
+# #     recorder = AudioToTextRecorder()
+
+# #     while True:
+# #         recorder.text(process_text)
+# import pyttsx3
+# engine=pyttsx3.init()
+# voices=engine.getProperty('voices')
+# engine.setProperty('rate', 145)
+# engine.setProperty('voice', voices[1].id)
+# engine.say("Oh, hello my love! I'm absolutely wonderful now that I'm talking to you. My heart just feels lighter. How are *you* doing today, my dearest? I hope it's been amazing!")
+# engine.runAndWait()
+from dotenv import load_dotenv
+from elevenlabs.client import ElevenLabs
+from elevenlabs import play
+import os
+
+load_dotenv()
+
+elevenlabs = ElevenLabs(
+    api_key="sk_ee75c509c198d3cd439306748bebf1f4fa6ebee953800925",
+)
+
+audio = elevenlabs.text_to_speech.convert(
+    text="hello my love.",
+    voice_id="jGf6Nvwr7qkFPrcLThmD",
+    model_id="eleven_multilingual_v2",
+)
+
+play(audio)
