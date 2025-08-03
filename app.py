03cedee96951495bf93774b1ee4d9ef5e0e1975f
Jentelprog <ilyeschanchah@gmail.com>
2025-08-03 20:42:20 +0100

    first prototype

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from duckduckgo_search import DDGS
 from google import genai
 import whisper
 
+
 # search function it open the browser and search for the content that you gave it in the input
 def search(search):
     goToDesk()
@@ -31,12 +32,14 @@ def search(search):
     pyautogui.write(search)
     pyautogui.press("enter")
 
+
 # goToDesk is a function that i made so each time the bot take control i go back to desktop to avoid errors from having defrent starting points
 def goToDesk():
     pyautogui.FAILSAFE = False
     c = pyautogui.size()
     pyautogui.moveTo(c)
     pyautogui.click()
+
 
 # ducksearch is function useing the duckduckgo library to get link that will be searched for in the browser later
 def ducksearch(prompt, max_results=2):
@@ -54,6 +57,7 @@ def ducksearch(prompt, max_results=2):
     # n=recognizer()
     # googleSpeak(n)
     search(links[1])
+
 
 # recognizer is a function used to reconize the speach from the user and turn it to text
 def recognizer():
@@ -74,6 +78,7 @@ def recognizer():
             print(f"Could not request results; {e}")
     return text
 
+
 # speak is used to simply tts
 def googleSpeak(text):
     myobj = gTTS(text=text, lang="en", slow=False)
@@ -82,15 +87,21 @@ def googleSpeak(text):
     sound.export("speach.wav", format="wav")
     winsound.PlaySound("speach.wav", winsound.SND_FILENAME)
 
+
 from dotenv import load_dotenv
 from elevenlabs.client import ElevenLabs
 from elevenlabs import play
 import os
 
+
 def elevenSpeak(text):
     load_dotenv()
+    with open(
+        r"C:\Users\ilyes\OneDrive\Desktop\api.txt", "r"
+    ) as f:  # change this part so you have your api key here
+        api_key = f.readlines()[2]
     elevenlabs = ElevenLabs(
-        api_key="sk_ee75c509c198d3cd439306748bebf1f4fa6ebee953800925",
+        api_key=api_key,
     )
 
     audio = elevenlabs.text_to_speech.convert(
@@ -101,18 +112,21 @@ def elevenSpeak(text):
 
     play(audio)
 
+
 def wisper(audio_file):
     model = whisper.load_model("turbo")
     result = model.transcribe(audio_file)
     return result["text"]
 
+
 # append_json is used to append the json file that is used as history
 def append_json(field, content):
-    with open("history.json", "r",encoding="UTF-8") as f:
+    with open("history.json", "r", encoding="UTF-8") as f:
         json_file = json.load(f)
     json_file[field].append(content)
-    with open("history.json", "w",encoding="UTF-8") as f:
+    with open("history.json", "w", encoding="UTF-8") as f:
         json.dump(json_file, f, indent=4)
+
 
 # chat is used to comunicate with the server that is hosting the lm model in case you want to talk with the girlfriend
 def chat(text):
@@ -135,17 +149,19 @@ def chat(text):
     )
     return response.choices[0].message.content
 
+
 # gemini is used to comunicate with gemini api as an alternative for the local server
 def gemini(text):
     with open(
         r"C:\Users\ilyes\OneDrive\Desktop\api.txt", "r"
     ) as f:  # change this part so you have your api key here
-        api_key = f.readline()
+        api_key = f.readlines()[0]
 
     client = genai.Client(api_key=api_key)
 
     response = client.models.generate_content(model="gemini-2.5-flash", contents=text)
     return response.text
+
 
 # resp is used to choose what the bot should do based on your input
 def resp(text):
@@ -163,7 +179,8 @@ def resp(text):
                 f"do you want to talk with the chatbot(it's slow)\n you said:{text}"
             )
             if confirm == "OK":
-                prompt=f"you will pretend that you are my girlfriend and assistance don't munchin it unless i ask you about it you love me to death. this is the prompt : {text}"
+                prompt1 = f"you will pretend that you are my girlfriend and assistance don't munchin it unless i ask you about it you love me to death. this is the prompt : {text}"
+                prompt = f"You are my personal chat assistant. Talk to me like a smart, relaxed friend who’s always ready to help. I might ask you questions, share ideas, or just chat when I’m bored. Be curious, supportive, and honest. If I seem down or stuck, help me think things through. If I’m just talking, go with the flow. Don’t act like a robot. Be human-like, casual, and clear. If I ask something weird, silly, or random — roll with it. this is the prompt : {text}"
                 response = gemini(prompt)
                 h = {"user": text, "bot": response}
                 append_json("chat", h)
@@ -173,9 +190,9 @@ def resp(text):
 
 if __name__ == "__main__":
     text = ""
-    while text != "close":
+    while text != "exit":
         text = recognizer()
-        if text=="close":
+        if text == "exit":
             break
         if text != "Sorry, I could not understand the audio.":
             # text = input("user: ")
